122ad78a92c19a0d264bd60b35c2f09d2943ff2a
Hyunsoo Seol <snow@cau.ac.kr>
2020-12-21 10:34:03 +0900

    Add files via upload

--- map.R
+++ map.R
@@ -1,11 +1,15 @@
 library(tidyverse)
 data <- read.csv("mix.csv")
+
+library(tictoc)
 ##############
+tic()
+
 boot.sample <- function(df) {
   df[sample(nrow(df), replace = TRUE),,]
 }
 
-boot <- map(1:100, ~ boot.sample(data)) 
+boot <- map(1:1000, ~ boot.sample(data)) 
 
 infit <- function(data){
   
@@ -23,6 +27,76 @@ infit <- function(data){
 boot.infit<- map_dfc(boot, ~ infit(data=.x))# data=.x 반드시 사용 
 infit.low <- apply(boot.infit, 1, stats::quantile,probs=.025)
 infit.high <- apply(boot.infit, 1, stats::quantile,probs=.975)
+
+
+toc()
+
+
+tic()
+####################################################
+
+boot.infit <- function(data, indices) {
+  d = data[indices,]
+  
+  # estimate Rasch model--------
+  res1 <-
+    mixRasch::mixRasch(
+      data = d,
+      steps = 1,
+      model = "RSM",
+      n.c = 1
+    )
+  
+  
+  # item infit--------
+  infit <- res1$item.par$in.out[, 1]
+  
+  return(infit)
+  
+}
+
+boot.in <-
+  boot::boot(data = data,
+             statistic = boot.infit,
+             R = 1000)
+
+infit <- boot.in$t
+
+infitlow = NA
+
+for (i in 1:ncol(data)) {
+  infitlow[i] <- stats::quantile(infit[, i], .025)
+  
+  
+}
+
+#infit lower--
+
+infitlow <- infitlow
+
+
+#infit high-
+
+infithigh = NA
+
+for (i in 1:ncol(data)) {
+  infithigh[i] <- stats::quantile(infit[, i], .975)
+  
+  
+}
+
+infithigh <- infithigh
+################################
+toc()
+
+
+
+
+
+
+
+
+
 
 
 
